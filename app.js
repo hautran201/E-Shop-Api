1fdfd6b85e9efe0894847a4553e58c87846c4046
DESKTOP-J039UH2\DELL <hautran.dev@gmail.com>
2023-11-30 21:34:13 +0700

    config send Mail activate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
@@ -11,6 +12,12 @@ const Router = require('./routers');
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
+
+//config cors
+var corsOptions = {
+    origin: '*',
+};
+app.use(cors(corsOptions));
 
 //config
 if (process.env.NODE_ENV !== 'PRODUCTION') {
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,27 +1,69 @@
 const path = require('path');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
-const userModel = require('../models/user.model');
+const User = require('../models/user.model');
 const ErrorHandler = require('../utils/ErrorHandler');
+const sendMail = require('../utils/sendMail');
 
 exports.register = async (req, res, next) => {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    const userEmail = await userModel.findOne({ email });
-    if (userEmail) {
-        return next(new ErrorHandler('User already exits!'));
+        const userEmail = await User.findOne({ email });
+        if (userEmail) {
+            const filename = req.file.filename;
+            const filePath = `public/${filename}`;
+
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({ message: 'Error deleting file' });
+                } else {
+                    res.json({ message: 'Deleted file successfully' });
+                }
+            });
+
+            return next(new ErrorHandler('User already exits!'));
+        }
+
+        const filename = req.file.filename;
+        const fileUrl = path.join(filename);
+
+        const user = {
+            name,
+            email,
+            password,
+            avatar: fileUrl,
+        };
+
+        const activationToken = createActivationToken(user);
+        const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+
+        try {
+            await sendMail({
+                email: user.email,
+                subject: 'Activate your account',
+                message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+            });
+
+            const newUser = await User.create(user);
+
+            res.status(201).json({
+                success: true,
+                message: `please check your email:- ${user.email} to activate your account!`,
+            });
+        } catch (err) {
+            return next(new ErrorHandler(err.message, 500));
+        }
+    } catch (err) {
+        return next(new ErrorHandler(err.message, 400));
     }
+};
 
-    if (!req.file) {
-        return next(new ErrorHandler('Please enter avatar', 400));
-    }
-    const filename = req.file.filename;
-
-    const user = {
-        name,
-        email,
-        password,
-        ...req.body,
-    };
-
-    res.send(user);
+//createActivationToken
+const createActivationToken = (user) => {
+    return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+        expiresIn: '5m',
+    });
 };
